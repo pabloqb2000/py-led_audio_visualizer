defd6a824e765d9511071226e06101a217bdfb71
polqb2000 <polqb2000@gmail.com>
2020-12-22 00:17:49 +0100

    Added rpi_ws281x support

---
+++ to_leds.py
@@ -0,0 +1,34 @@
+from rpi_ws281x import *
+
+# LED strip configuration:
+LED_COUNT      = 16      # Number of LED pixels.
+LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
+#LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
+LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+strip = None
+
+'''
+    Given:
+        - frame: an iterable with n triplets of RGB (0-255 (int)) colors
+    Lights the leds based on this colors
+'''
+def frame_to_leds(frame):
+    if not strip:
+        return 
+    for i, value in enumerate(frame):
+        strip.setPixelColor(i, value)
+        strip.show()
+
+'''
+    On start initialize strip
+'''
+# Create NeoPixel object with appropriate configuration.
+strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+# Intialize the library (must be called once before other functions).
+strip.begin()
+
+    
