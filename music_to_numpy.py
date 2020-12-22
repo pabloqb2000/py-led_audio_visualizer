f958a219760e4a8fcfd6e9240f98a547a0392cef
polqb2000 <polqb2000@gmail.com>
2020-12-22 01:10:11 +0100

    More .npy changes

--- music_to_numpy.py
+++ music_to_numpy.py
@@ -2,16 +2,18 @@ from colour import Color
 from video_maker import *
 from animation_maker import *
 
-in_file = './Songs/Song02.wav'
+song = 1
+in_file = f'./Songs/Song0{song}.wav'
 c1, c2 = Color('#fceaa8'), Color('#00e5ff')
 #c1, c2 = Color('#000000'), Color('#00fffb')
 c3, c4 = Color('#000000'), Color('#000000')
 gradient_mode = 'hsl'
 n_points = 100
-anim_mode = 'fft 2000 8192 1'
+# anim_mode = 'fft 2000 8192 1'
+anim_mode = 'volume_bar sim bounce'
 fps = 30
 
-out_file = './npys/song01.npy' # will still be a .avi file
+out_file = f'./npys/song0{song}.npy' # will still be a .avi file
 
 point_values = make_animation(in_file, anim_mode, n_points, fps, c1, c2, c3, c4, gradient_mode)
 
